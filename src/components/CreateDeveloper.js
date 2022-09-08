c9884099cd5d18c30701ce50d51572c1fb334d9a
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 19:26:55 +0300

    CreateDeveloper

--- src/components/CreateDeveloper.js
+++ src/components/CreateDeveloper.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const CreateDeveloper = () => {
+const CreateDeveloper = ({handleNewDeveloper}) => {
   return (
     <div>
-      
+      <h1> Create A Developer</h1>
+            <label> Developer Name:
+                <input type="text" placeholder="Developer Name"></input>
+            </label>
+            <button onClick={handleAddDeveloper}>Add</button>
     </div>
   )
 }
