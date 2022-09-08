7a15bbf1e65bec95b24063321f73e0ec8f3b2d6e
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 21:19:28 +0300

    EntryScreen

--- src/components/EntryScreen.js
+++ src/components/EntryScreen.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import CreateEntry from './CreateEntry'
 
 const EntryScreen = () => {
   return (
     <div>
-      
+      <CreateEntry addEntry={addEntry} allDevelopers={allDevelopers}/>
+       {entries.map((entry, index)=>
+         <div key={index}>
+            <p><strong>API: </strong>{entry.API}</p>
+            <p><strong>Description:</strong>{defineSuccess(entry.Description)}</p>
+            <p><strong>Link:</strong> {entry.Link}</p>
+            <p><strong>Category:</strong> {entry.Category}</p>
+            <p><strong>Developer:</strong>{entry.developer.developer_name}</p>
+            <button onClick={()=>deleteLaunch(entry)}>Delete API</button>
+            <hr></hr>
+         </div> 
+       )}
     </div>
   )
 }
