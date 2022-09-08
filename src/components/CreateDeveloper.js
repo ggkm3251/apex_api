4f185c1f45650b9f676f230da99829cc48abc8aa
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 22:02:37 +0300

    CreateDeveloper.js

--- src/components/CreateDeveloper.js
+++ src/components/CreateDeveloper.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
 const CreateDeveloper = ({handleNewDeveloper}) => {
+
+  function handleAddDeveloper(){
+    const developer = {
+        developer_name: document.querySelector("#root > div > div > div:nth-child(2) > div:nth-child(1) > label > input[type=text]").value
+    }
+    console.log(developer)
+    document.querySelector("#root > div > div > div:nth-child(2) > div:nth-child(1) > label > input[type=text]").value = ''
+    
+    fetch("http://localhost:9292/developers",{
+        method: "POST",
+        headers:{
+            "content-type" : "application/json",
+            "accept" : "application/json"
+        },
+        body: JSON.stringify(developer)
+    })
+    .then(resp => resp.json())
+    .then(resp => handleNewDeveloper(resp))
+    
+}
+
   return (
     <div>
       <h1> Create A Developer</h1>
