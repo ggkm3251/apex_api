401b1ef04142e15518ed0ba8f550255c7a9ccec8
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 19:00:16 +0300

    home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Developers from './Developers'
 
 const Home = () => {
   return (
     <div>
-      
+      <h1>Developer Api Entries</h1>
+          <Developers handleNewCompany={handleNewDeveloper} deleteDeveloper={deleteDeveloper} allCompanies={allDevelopers}/>
+          <EntryScreen allDevelopers={allDevelopers}/>
     </div>
   )
 }
