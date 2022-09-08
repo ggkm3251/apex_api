216081ad1f6a74bfc74713437bc7727553e1d49e
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 21:46:49 +0300

    EntryScreen.js

--- src/components/EntryScreen.js
+++ src/components/EntryScreen.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import CreateEntry from './CreateEntry'
 
-const EntryScreen = () => {
+const EntryScreen = ({allDevelopers}) => {
+
+  const [entries, setEntries] = useState([])
+
+    useEffect(()=>{
+      fetch("http://localhost:9292/entries")
+      .then((r) => r.json())
+      .then(entries => setEntries(entries))
+    },[])
+
+    function deleteEntry({id}){
+        setLaunches(entries.filter(entry => entry.id !== id))
+        fetch(`http://localhost:9292/entries/${id}`,{
+            method: 'DELETE'
+        })
+            .then(resp => resp.json())
+            .then()
+    }
+    
+    function addEntry(newEntry){ 
+        setEntries([...entries, newEntry])
+    }
+
   return (
     <div>
       <CreateEntry addEntry={addEntry} allDevelopers={allDevelopers}/>
        {entries.map((entry, index)=>
          <div key={index}>
             <p><strong>API: </strong>{entry.API}</p>
-            <p><strong>Description:</strong>{defineDescription(entry.Description)}</p>
+            <p><strong>Description:</strong>{entry.Description}</p>
             <p><strong>Link:</strong> {entry.Link}</p>
             <p><strong>Category:</strong> {entry.Category}</p>
             <p><strong>Developer:</strong>{entry.developer.developer_name}</p>
