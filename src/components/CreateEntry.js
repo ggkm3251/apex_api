63017993d605ed9a1ae0049b303233b61854a1ac
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 21:58:43 +0300

    CreateEntry.js

--- src/components/CreateEntry.js
+++ src/components/CreateEntry.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
-const CreateEntry = () => {
+const CreateEntry = ({addEntry, allDevelopers}) => {
+
+  function handleSubmit (e){
+    e.preventDefault()
+        const newEntry = {
+            API : e.target[0].value,
+            Description: e.target[2].value,
+            Link: e.target[1].value,
+            developerName: e.target[3].value
+        }
+        e.target.reset()
+    fetch("http://localhost:9292/entries",{
+        method: "POST",
+        headers:{
+            "content-type" : "application/json",
+            "accept" : "application/json"
+        },
+        body: JSON.stringify(newEntry)
+    })
+    .then(resp => resp.json())
+    .then(resp => addEntry(resp))
+  }
   return (
     <div>
       <h1>Create An API</h1>
