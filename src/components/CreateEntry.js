f92065c2dce18620741f1dfe4f0dbea006c31fb6
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-09-08 19:46:45 +0300

    CreateEntry.js

--- src/components/CreateEntry.js
+++ src/components/CreateEntry.js
@@ -3,7 +3,20 @@ import React from 'react'
 const CreateEntry = () => {
   return (
     <div>
-      
+      <h1>Create An API</h1>
+          <form onSubmit={handleSubmit}>
+            <label>Entry Name:</label>
+                <input type='text' name="entryName" placeholder='Entry Name'></input>
+            <label>Description:</label>
+            <input type='text' name="description" placeholder='Description'></input>
+            <label>Link:</label>
+                <input type='text' name='link' placeholder="Link"></input>
+            <label>Developer Name:</label>
+                <select>
+                    {allDevelopers.map((developer, index) => <option key={index}>{developer.developer_name}</option>)}   
+                </select>    
+                <input type='submit'></input>
+          </form>
     </div>
   )
 }
